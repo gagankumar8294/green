727fb8fa0ca09c23c3cf4bdcc30cf37aa2f3e108
gagankumar8294 <gagankumar8294@gmail.com>
2025-12-26 13:07:37 +0530

    link change

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
   <h3>Useful Links</h3>
   <ul>
     <li><a href="/">Home</a></li>
-    <li><a href="/plants">Plants</a></li>
+    <li><a href="/shop">Plants</a></li>
     <li><a href="/about">About Us</a></li>
     <li><a href="/contact">Contact Us</a></li>
   </ul>
