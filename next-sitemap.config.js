7bc6d107be58d1983d6f17a3c0640328f1b3d94f
gagankumar8294 <gagankumar8294@gmail.com>
2025-12-27 21:47:17 +0530

    working with sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.happygreenery.in/",
+  generateRobotsTxt: false,
+  exclude: [
+    "/admin",
+    "/my-orders",
+  ],
+};
